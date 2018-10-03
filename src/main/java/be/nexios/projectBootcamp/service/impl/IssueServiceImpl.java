3a50f2eb8989b8542fe54ccf6e4ebb19dfb2bd67
Niels De Stickere <nds@nexiosit.com>
2018-10-03 14:59:00 +0200

    tekstje

--- src/main/java/be/nexios/projectBootcamp/service/impl/IssueServiceImpl.java
+++ src/main/java/be/nexios/projectBootcamp/service/impl/IssueServiceImpl.java
@@ -1,4 +1,36 @@
 package be.nexios.projectBootcamp.service.impl;
 
-public class IssueServiceImpl {
+import be.nexios.projectBootcamp.domain.Issue;
+import be.nexios.projectBootcamp.service.IssueService;
+import be.nexios.projectBootcamp.service.dto.IssueDTO;
+import org.bson.types.ObjectId;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public class IssueServiceImpl implements IssueService {
+
+    @Override
+    public Mono<IssueDTO> getIssue(ObjectId id) {
+        return null;
+    }
+
+    @Override
+    public Flux<IssueDTO> getAllIssues() {
+        return null;
+    }
+
+    @Override
+    public Mono<ObjectId> createIssue(Issue issue) {
+        return null;
+    }
+
+    @Override
+    public Mono<Void> deleteIssue(ObjectId id) {
+        return null;
+    }
+
+    @Override
+    public Mono<Void> updateIssue(ObjectId id, Issue issue) {
+        return null;
+    }
 }
--- src/main/java/be/nexios/projectBootcamp/service/impl/UserServiceImpl.java
+++ src/main/java/be/nexios/projectBootcamp/service/impl/UserServiceImpl.java
@@ -1,4 +1,34 @@
 package be.nexios.projectBootcamp.service.impl;
 
-public class UserServiceImpl {
+import be.nexios.projectBootcamp.service.UserService;
+import be.nexios.projectBootcamp.service.dto.UserDTO;
+import org.bson.types.ObjectId;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public class UserServiceImpl implements UserService {
+    @Override
+    public Mono<ObjectId> createUser(UserDTO user) {
+        return null;
+    }
+
+    @Override
+    public Mono<UserDTO> getUser(ObjectId id) {
+        return null;
+    }
+
+    @Override
+    public Flux<UserDTO> getAllUsers() {
+        return null;
+    }
+
+    @Override
+    public Mono<Void> updateUser(ObjectId id, UserDTO user) {
+        return null;
+    }
+
+    @Override
+    public Mono<Void> deleteUser(ObjectId id) {
+        return null;
+    }
 }
