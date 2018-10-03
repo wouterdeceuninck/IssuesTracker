e08bf2666fc76d90a5bcdb7076c10cbfa601e568
michiel-veulemans <michiel.veulemans@hotmail.com>
2018-10-03 16:04:30 +0200

    dbref annotation

--- src/main/java/be/nexios/projectBootcamp/domain/Project.java
+++ src/main/java/be/nexios/projectBootcamp/domain/Project.java
@@ -4,8 +4,10 @@ import lombok.Builder;
 import lombok.Data;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.List;
 import java.util.UUID;
 
 @Data
@@ -16,4 +18,6 @@ public class Project {
     ObjectId id;
     String name;
     String description;
+    @DBRef
+    List<Issue> issues;
 }
